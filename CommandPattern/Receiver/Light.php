c11c6aa5dc02b2cc66000fed80f535750876a5f4
Jovry-Lee <364960204@qq.com>
2019-05-24 18:00:30 +0800

    设计模式

---
+++ CommandPattern/Receiver/Light.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace CommandPattern\Receiver;
+
+class Light
+{
+    public $location;
+
+    public function __construct($location = '')
+    {
+        $this->location = $location;
+    }
+
+    public function on()
+    {
+        // todo 实现开灯.
+        echo "{$this->location} Light is On\n";
+    }
+
+    public function off()
+    {
+        // todo 实现关灯.
+        echo "{$this->location} Light is Off\n";
+    }
+}
